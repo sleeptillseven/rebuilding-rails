db28f1ae4dcc824e1d6e14daa8a046eeeb0b8927
Christoph Jasinski <christoph.jasinski@googlemail.com>
2013-02-07 06:49:04 +0100

    Filter requests for favicon

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -5,11 +5,18 @@ require "rulers/routing"
 module Rulers
 
   class Application
+    HTML    = 'text/html'
+    CONTENT = 'Content-Type'
+
     def call(env)
+      if env['PATH_INFO'][/favicon\.ico/]
+        return [404, { CONTENT => HTML }, []]
+      end
+
       klass, action = get_controller_and_action(env)
-      controller = klass.new
-      text = controller.send(action)
-      [200, {'Content-Type' => 'text/html'}, [text]]
+      controller    = klass.new
+      text          = controller.send(action)
+      [200, { CONTENT => HTML }, [text]]
     end
   end
 
