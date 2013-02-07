ad8a8e8779a2ffceeed6744f4303ac189485597f
Christoph Jasinski <christoph.jasinski@googlemail.com>
2013-02-07 18:27:10 +0100

    Add get? and post? methods to controllers

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -26,5 +26,13 @@ module Rulers
     def initialize(env)
       @env = env
     end
+
+    def get?
+      !!env['REQUEST_METHOD'][/get/i]
+    end
+
+    def post?
+      !!env['REQUEST_METHOD'][/post/i]
+    end
   end
 end
